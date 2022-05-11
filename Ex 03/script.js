7da2b3dd1a5a9a94d747f68de2d6a374deb61345
LeppoXena <francesco.lepore@xenagames.com>
2022-05-11 19:04:13 +0200

    Ex 03 completed

--- Ex 03/script.js
+++ Ex 03/script.js
@@ -1,25 +1,28 @@
-// crea 2 array, di 5 stringhe il primo e di 2 stringhe il secondo, stampa dentro una UL dove ogni LI ha come contenuto principale in ordine, le parole del primo array e come sotto-contenuto annidato le 2 parole del secondo array in una UL annidata, tipo:
-// <ul>
-//  	<li>parola1 arr1
-// 		<ul>
-//  			<li>parola1 arr2</li>
-// 			<li>parola2 arr2</li>
-// 		</ul>
-// 	</li>
-// 	<li>parola2 arr1
-// 		<ul>
-//  			<li>parola1 arr2</li>
-// 			<li>parola2 arr2</li>
-// 		</ul>
-// 	</li>
-// 	<li>parola3 arr1
-// 	…etc
+/* 
+crea 2 array, di 5 stringhe il primo e di 2 stringhe il secondo, stampa dentro una UL dove ogni LI ha come contenuto principale in ordine, le parole del primo array e come sotto-contenuto annidato le 2 parole del secondo array in una UL annidata, tipo:
+<ul>
+    <li>parola1 arr1
+        <ul>
+            <li>parola1 arr2</li>
+		    <li>parola2 arr2</li>
+	    </ul>
+    </li>
+    <li>parola2 arr1
+        <ul>
+            <li>parola1 arr2</li>
+            <li>parola2 arr2</li>
+        </ul>
+    </li>
+    <li>parola3 arr1
+…etc
+*/
 
+// creo i due array
 
-// creo l'array vuoto
+const plantList =["Oleandro", "Baobab", "Toto", "Broken", "Dreams"];
+const placeList =["Africa", "Giardino"];
 
-const plantList =["Oleandro", "Baobab", "Sandalo", "Zucca", "Rododendro"];
-const plantInfo =["Ciclo vitale", "Coltivazione"];
+// lego la lista dell'html alla costante
 
 const plantUl = document.getElementById("list_output");
 
@@ -31,11 +34,24 @@ for (let i = 0; i < plantList.length; i++) {
 
     let plantLi = document.createElement("li");
 
+    plantLi.append(plant);
+
     plantUl.append(plantLi);
 
+    let placeUi = document.createElement("ul");
 
+    for (let j = 0; j < placeList.length; j++) {
 
+        const place = placeList[j];
 
+        let placeLi = document.createElement("li");
 
+        placeLi.append(place);
+
+        placeUi.append(placeLi);
+
+        plantLi.append(placeUi);
+
+    }        
 }
 
